4d4edca96afa6cfb58ffd09bcceccbaed803e44d
John Morales <jmorales@gmail.com>
2014-05-02 09:02:22 -0400

    Update chef_colorize.sh

--- chef_colorize.sh
+++ chef_colorize.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/awk -f
+/INFO:/ { 
+  print "[0;32m", $0, "[0m"
+  next
+}
+/WARN:/ { 
+  print "[1;31m", $0, "[0m"
+  next
+}
+/ERROR:/ { 
+  print "[0;35m", $0, "[0m"
+  next
+}
+/DEBUG:/ { 
+  print "[0;30m", $0, "[0m"
+  next
+}
+/FATAL:/ { 
+  print "[0;31m", $0, "[0m"
+  next
+}
 {
-  print $0
+  print
 }
