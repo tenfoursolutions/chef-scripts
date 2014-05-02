e6cef1bbe8bc6170c58837a462c12097f1cc91bb
JohnMorales <jmorales@gmail.com>
2014-05-01 20:48:51 -0400

    No progress in curl

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl https://github.com/tenfoursolutions/chef-scripts/raw/master/chef_colorize.sh -o /usr/local/bin/chef_colorize
+curl -s https://github.com/tenfoursolutions/chef-scripts/raw/master/chef_colorize.sh -o /usr/local/bin/chef_colorize
 chmod +x /usr/local/bin/chef_colorize
-curl https://github.com/tenfoursolutions/chef-scripts/raw/master/run_chef.sh -o /usr/local/bin/run_chef
+curl -s https://github.com/tenfoursolutions/chef-scripts/raw/master/run_chef.sh -o /usr/local/bin/run_chef
 chmod +x /usr/local/bin/run_chef
